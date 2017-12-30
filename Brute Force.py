9c69abbf101cfdcc1a2d1c8d625ec13b8ed72117
Frestho <34761944+Frestho@users.noreply.github.com>
2017-12-30 12:58:20 -0800

    Better loops #4

--- Brute Force.py
+++ Brute Force.py
@@ -5,22 +5,22 @@ code = input('Put your encoded Caesar Cipher here: ')
 for key in range (0, 25):
     holder = ''
     #this loop goes through every character in the encoded phrase and decodes it accordingly
-    for place in range (0, len(code)):
+    for char in code:
         #****
         #this part of the code handles capital letters.
-        inCaps = caps.find(code[place]) #if there is a capital letter, this line finds its position in the alphabet
+        inCaps = caps.find(char) #if there is a capital letter, this line finds its position in the alphabet
         if inCaps < 26 and inCaps > -1: #should only be true if there was a capital letter
             code2 = ''#this variable handles some temporary stuff
-            for i in range (0, len(code)): #goes through the user input, ready to replace the capital letter with a lowercase
-                if i == place: #am I at the capital letter? if so add it to code 2 from the lowercase letter list
+            for char2 in code: #goes through the user input, ready to replace the capital letter with a lowercase
+                if char2 == char: #am I at the capital letter? if so add it to code 2 from the lowercase letter list
                     code2 += letters[inCaps]
                 else: #otherwise, do nothing different
-                    code2 += code[i]
+                    code2 += char2
             code = code2 
         #****
-        placeInLetters = letters.find(code[place])
+        placeInLetters = letters.find(char)#if the current character is a letter, find its position in the letters variable. Then the next part of the code will change the letter using the position and the key
         if not(placeInLetters < 26 and placeInLetters > -1):
-            holder = holder + code[place]
+            holder = holder + char
         else:
             holder = holder + letters[(placeInLetters+key) % 26]
     print (holder)
