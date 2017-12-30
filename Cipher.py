7be558eefb9061f121d36d0952f579f6ba5e6493
Frestho <34761944+Frestho@users.noreply.github.com>
2017-12-30 12:56:23 -0800

    Better Loops #3
    Done to Cipher now.

--- Cipher.py
+++ Cipher.py
@@ -1,24 +1,24 @@
 import random
 #sets the caesar cipher key
 key = random.randint (1, 25)
-print (key)
+print (key)#gives you the key, remove this for the key to be secret from the user
 letters = 'abcdefghijklmnopqrstuvwxyz'
 caps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 phrase = input('Type a phrase to be encoded: ')
 encoded = ''
-for place in range(0, len(phrase)):
-    inCaps = caps.find(phrase[place])
+for char in phrase:
+    inCaps = caps.find(char)
     if inCaps < 26 and inCaps > -1: #should only be true if there was a capital letter
             phrase2 = ''#this variable handles some temporary stuff
-            for i in range (0, len(phrase)): #goes through the user input, ready to replace the capital letter with a lowercase
-                if i == place: #am I at the capital letter? if so add it to code 2 from the lowercase letter list
+            for char2 in phrase: #goes through the user input, ready to replace the capital letter with a lowercase
+                if char == char2: #am I at the capital letter? if so add it to code 2 from the lowercase letter list
                     phrase2 += letters[inCaps]
                 else: #otherwise, do nothing different
-                    phrase2 += phrase[i]
+                    phrase2 += char2
             phrase = phrase2 
-    placeInLetters = letters.find(phrase[place])
+    placeInLetters = letters.find(char)#if the current character is a letter, find its position in the letters variable. Then the next part of the code will change the letter using the position and the key
     if not(placeInLetters < 25 and placeInLetters > -1):
-        encoded = encoded + phrase[place]
+        encoded = encoded + char
     else:
         encoded = encoded + letters[(placeInLetters+key) % 26]
-print (encoded)    
+print(encoded)    
