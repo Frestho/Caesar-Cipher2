3249e936b99897c0501379b482f6e064db38609a
Frestho <34761944+Frestho@users.noreply.github.com>
2017-12-30 13:04:28 -0800

    Better loops #5

--- Frequency Analysis.py
+++ Frequency Analysis.py
@@ -13,23 +13,25 @@ def char_frequency(str):
                 dict[n] = 1
     return dict
 freq = max(zip(char_frequency(code).values(), char_frequency(code).keys()))[1]
-print (freq)
-key = letters.find(freq)-4
-print (key)
+key = letters.find(freq)-4#4 is the index of the letter e
+print (key)#prints what the program thinks the key is
 holder = ''
-for place in range(0, len(code)):    
-    inCaps = caps.find(code[place])
-    if inCaps < 26 and inCaps > -1:
-        code2 = ''
-        for i in range (0, len(code)):
-            if i == place:
-                code2+=letters[inCaps]
-            else:
-                code2+=code[i]
-        code = code2
-    placeInLetters = letters.find(code[place])
-    if not(placeInLetters < 26 and placeInLetters > -1):
-        holder = holder + code[place]
-    else:
-        holder = holder + letters[(placeInLetters-key) % 26]
+for char in code:
+        #****
+        #this part of the code handles capital letters.
+        inCaps = caps.find(char) #if there is a capital letter, this line finds its position in the alphabet
+        if inCaps < 26 and inCaps > -1: #should only be true if there was a capital letter
+            code2 = ''#this variable handles some temporary stuff
+            for char2 in code: #goes through the user input, ready to replace the capital letter with a lowercase
+                if char2 == char: #am I at the capital letter? if so add it to code 2 from the lowercase letter list
+                    code2 += letters[inCaps]
+                else: #otherwise, do nothing different
+                    code2 += char2
+            code = code2 
+        #****
+        placeInLetters = letters.find(char)#if the current character is a letter, find its position in the letters variable. Then the next part of the code will change the letter using the position and the key
+        if not(placeInLetters < 26 and placeInLetters > -1):
+            holder = holder + char
+        else:
+            holder = holder + letters[(placeInLetters-key) % 26]
 print (holder)
