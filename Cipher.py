e2b00939169c854d23ff7d268351d4e6f70e7005
Frestho <34761944+Frestho@users.noreply.github.com>
2017-12-30 18:15:28 -0800

    h

--- Cipher.py
+++ Cipher.py
@@ -8,10 +8,11 @@ phrase = input('Type a phrase to be encoded: ')
 encoded = ''
 for char in phrase:
     inCaps = caps.find(char)
+    print (inCaps)
     if inCaps < 26 and inCaps > -1: #should only be true if there was a capital letter
             phrase2 = ''#this variable handles some temporary stuff
             for char2 in phrase: #goes through the user input, ready to replace the capital letter with a lowercase
-                if char == char2: #am I at the capital letter? if so add it to code 2 from the lowercase letter list
+                if char2 == char: #am I at the capital letter? if so add it to code 2 from the lowercase letter list
                     phrase2 += letters[inCaps]
                 else: #otherwise, do nothing different
                     phrase2 += char2
