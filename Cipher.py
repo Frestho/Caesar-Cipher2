302b75432e91b543114b232c237b2f639cbac3b1
Frestho <34761944+Frestho@users.noreply.github.com>
2017-12-30 18:20:29 -0800

    Capital letter handling
    Way more efficient.

--- Cipher.py
+++ Cipher.py
@@ -8,15 +8,8 @@ phrase = input('Type a phrase to be encoded: ')
 encoded = ''
 for char in phrase:
     inCaps = caps.find(char)
-    print (inCaps)
     if inCaps < 26 and inCaps > -1: #should only be true if there was a capital letter
-            phrase2 = ''#this variable handles some temporary stuff
-            for char2 in phrase: #goes through the user input, ready to replace the capital letter with a lowercase
-                if char2 == char: #am I at the capital letter? if so add it to code 2 from the lowercase letter list
-                    phrase2 += letters[inCaps]
-                else: #otherwise, do nothing different
-                    phrase2 += char2
-            phrase = phrase2 
+        char = letters[inCaps]
     placeInLetters = letters.find(char)#if the current character is a letter, find its position in the letters variable. Then the next part of the code will change the letter using the position and the key
     if not(placeInLetters < 25 and placeInLetters > -1):
         encoded = encoded + char
